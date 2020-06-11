7459bd1a870ea5cff09e9730fd3cc3646d053f14
luisschmitz <luis.schmitz@web.de>
2020-06-11 12:04:52 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,19 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-function updateSubtotal($product) {
-  // Iteration 1.1
+function updateSubTotal(product) {
+  let itemPrice = product.querySelector(".pu span").innerHTML;
+  let quantity = product.querySelector(".qty label input").value;
+  let subtotal = itemPrice * quantity;
+  return (product.querySelector(".subtot span").innerHTML = subtotal);
 }
-
-function calculateAll() {
-  // Iteration 1.2
+function calcAll() {
+  console.log(document.querySelectorAll(".product"));
+  let totalPrice = 0;
+  document.querySelectorAll(".product").forEach((product) => {
+    console.log("for each", product);
+    totalPrice += updateSubTotal(product);
+  });
+  document.querySelector("h2 span").innerHTML = totalPrice;
 }
-$calc.onclick = calculateAll;
+calc.onclick = calcAll;
